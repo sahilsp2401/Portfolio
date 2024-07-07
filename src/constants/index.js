71ea7354cab7287b5a7502c0080d02ac773d54eb
Sahil Sapkal <168254910+sahilsp2401@users.noreply.github.com>
2024-07-07 19:21:38 +0530

    Information Update

--- src/constants/index.js
+++ src/constants/index.js
@@ -70,7 +70,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Rajmudra Society,Dhankawadi,Pune Maharashtra India",
+  phoneNo: "+91 8308998833",
+  email: "sahilsapkal2401@gmail.com",
 };
