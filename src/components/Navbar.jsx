dae4ec04acab2814ac03550ee718fa8f4ad64506
Sahil Sapkal <168254910+sahilsp2401@users.noreply.github.com>
2024-08-07 22:05:18 +0530

    Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from "../assests/kevinRushLogo.png"
+import logo from "../assests/sahil.png"
 import { FaLinkedin } from 'react-icons/fa'
 import { FaGithub } from 'react-icons/fa'
 import { FaInstagram } from 'react-icons/fa'
@@ -20,7 +20,7 @@ const Navbar = () => {
   return (
     <motion.nav whileInView={{opacity:1,y:0}} initial={{opacity:0}} transition={{duration:1}}  className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-            <img className='mx-2 w-10' src={logo} alt="logo" />
+            <img className='mx-2 w-20' src={logo} alt="logo" />
         </div>
         <div className='m-8 flex items-center justify-center gap-4 text-2xl cursor-pointer'>
             <FaLinkedin onClick={()=>handleClick("linkedin")}/>
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,69 +3,69 @@
 // import project3 from "../assets/projects/project-3.jpg";
 // import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `Passionate about technology and problem-solving, with a strong foundation in software engineering.Academic experience includes developing a variety of projects using different technologies.Eager to apply academic learning and creative problem-solving skills to real-world challenges in software development.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL,Django and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and playing Cricket and watching Anime.`;
 
 export const EXPERIENCES = [
   {
     year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    role: "Master of Computer Application",
+    company: "Sinhgad Institute of Management",
+    description: `8.68 SGPA`,
     technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    year: "2019 - 2022",
+    role: "Bachelor of Science in Animation",
+    company: "Fergusson College of Arts and Science",
+    description: `8.27 CGPA`,
     technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
   },
   {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    year: "2018 - 2019",
+    role: "Higher Secondary Certificate",
+    company: "Crescent High School and Jr College",
+    description: `83%`,
     technologies: ["Python", "Svelte", "Three.js", "Postgres"],
   },
   {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    year: "2016 - 2017",
+    role: "Secondary School Certificate",
+    company: "Priyadarshini English School",
+    description: `85%`,
     technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "CardMintCraft",
     // image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "Created an online generator for visiting cards, resumes, and ID cards using Django,      	HTML, CSS,   JavaScript, and Bootstrap. Users input details, select designs, and download PDFs.",
+    technologies: ["Django", "HTML", "CSS", "JavaScript", "Bootstrap"],
   },
   {
-    title: "Task Management App",
+    title: "Minigram",
     // image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "Developed a social media website using the MERN stack (MongoDB, Express, ReactJs, 	Node.js). The platform allows users to create profiles, post updates, like and comment on posts. It also 	includes features like user authentication.",
+    technologies: ["MongoDB", "Express", "ReactJs"," Node.js"],
   },
   {
     title: "Portfolio Website",
     // image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+    technologies: ["HTML", "CSS", "React", "Tailwind"],
   },
   {
-    title: "Blogging Platform",
+    title: "NewsWebsite",
     // image: project4,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "NewsWebsite is a React.js-based application that displays the latest news articles from 	various categories with an infinite scroll feature. Users can view news articles sorted by categories such as 	Business, Entertainment, Health, Science, Sports, and Technology.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
   },
 ];
 
